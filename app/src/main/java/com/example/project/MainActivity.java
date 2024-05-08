9586a7470fb2f032a4eeeedb3660285f956c704d
a23jonam <a23jonam@student.his.se>
2024-05-08 16:00:59 +0200

    added new TypeToken to onPostExecute

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -9,9 +9,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
@@ -52,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     @Override
     public void onPostExecute(String json) {
         Gson gson = new Gson();
+
+        Type type = new TypeToken<List<RecyclerViewItem>>() {}.getType();
 
         adapter.updateData(items);
 
