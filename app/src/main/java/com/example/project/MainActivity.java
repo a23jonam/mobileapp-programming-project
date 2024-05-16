ddf74ef4ce8b2f5d124d657859b5567ad4544b13
a23jonam <a23jonam@student.his.se>
2024-05-16 14:23:30 +0200

    added code from a previous assignment to add a button to the main activity that brings the user to the second activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +36,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button secondActivityButton=findViewById(R.id.secondActivityButton);
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("extra", "come!");
+                startActivity(intent);
+            }});
 
         gson = new Gson();
 
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("extra");
+            TextView textView1 = findViewById(R.id.intentView);
+            textView1.setText("The age of men is over, the time of the droid has ".concat(name));
+        }
+
     }
 }
